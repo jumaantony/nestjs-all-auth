cf0837111edcd5c5884f0c160717eb1cce88b87e
Juma Antony <jumaantony840@gmail.com>
2023-09-01 09:17:07 +0300

    setting up project

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import {
+  Module,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env/.env`,
+    }),
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    },
+  ],
+})
+export class AppModule {}
