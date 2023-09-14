4d8d50322add850707d06bdfc3868854b1dcb307
Juma Antony <jumaantony840@gmail.com>
2023-09-14 12:11:02 +0300

    running lint

--- src/supabase/supabase.types.ts
+++ src/supabase/supabase.types.ts
@@ -4,53 +4,53 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export interface Database {
   public: {
     Tables: {
       users: {
         Row: {
-          created_at: string | null
-          first_name: string | null
-          id: string
-          last_name: string | null
-          password_hash: string | null
-          phone: string | null
-          updated_at: string | null
-        }
+          created_at: string | null;
+          first_name: string | null;
+          id: string;
+          last_name: string | null;
+          password_hash: string | null;
+          phone: string | null;
+          updated_at: string | null;
+        };
         Insert: {
-          created_at?: string | null
-          first_name?: string | null
-          id?: string
-          last_name?: string | null
-          password_hash?: string | null
-          phone?: string | null
-          updated_at?: string | null
-        }
+          created_at?: string | null;
+          first_name?: string | null;
+          id?: string;
+          last_name?: string | null;
+          password_hash?: string | null;
+          phone?: string | null;
+          updated_at?: string | null;
+        };
         Update: {
-          created_at?: string | null
-          first_name?: string | null
-          id?: string
-          last_name?: string | null
-          password_hash?: string | null
-          phone?: string | null
-          updated_at?: string | null
-        }
-        Relationships: []
-      }
-    }
+          created_at?: string | null;
+          first_name?: string | null;
+          id?: string;
+          last_name?: string | null;
+          password_hash?: string | null;
+          phone?: string | null;
+          updated_at?: string | null;
+        };
+        Relationships: [];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
 }
